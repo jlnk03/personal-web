e13939cbd035fde720bca64d458a7bf36d752760
jlnk03 <95719434+jlnk03@users.noreply.github.com>
2023-10-21 16:41:43 +0200

    sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,40 @@
+export default function sitemap() {
+    return [
+      {
+        url: 'https://julianlnk.com',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 1,
+      },
+      {
+        url: 'https://julianlnk.com/about',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.6,
+      },
+      {
+        url: 'https://julianlnk.com/projects',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://julianlnk.com/projects/swinglab',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://julianlnk.com/projects/muunai',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://julianlnk.com/impressum',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 0,
+      },
+    ]
+  }
